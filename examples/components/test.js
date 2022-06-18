4ba697756183dddf8dbf275bdddac915a9ef4ecc
alex.zhang <gzane24@hotmail.com>
2022-06-18 13:41:34 +0800

    Updates

--- examples/components/test.js
+++ examples/components/test.js
@@ -1,3 +1,23 @@
-window.slider.once('ready', () => {
-  console.log("welcome to slider");
+window.slider.once('onStoreDataLoaded', (evt) => {
+  console.log('event: onStoreDataLoaded');
+  const storeData = evt.detail;
+  if (!storeData) {
+    return;
+  }
+
+  const appendData = {
+    hello: 'from script'
+  }
+
+  storeData.appendData = appendData;
+});
+
+window.slider.once('onSchemaInitial', () => {
+  console.log("event: onSchemaInitial");
 })
+
+window.slider.once('onLoaded', () => {
+  console.log("event: onLoaded");
+})
+
+// window.slider.throwError(new Error('asdadasd'));
